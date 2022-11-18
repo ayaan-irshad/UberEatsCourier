e5f9be1b82cd88ebf2b07dec2894b5294d920785
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-11-18 23:55:33 +0530

    loading order details from the orders.json

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 
+// export default function App() {
+//   return (
+//     <View style={styles.container}>
+//       <Text>Open up App.js to start working on your app!</Text>
+//       <StatusBar style="auto" />
+//     </View>
+//   );
+// }
+
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image } from "react-native";
+import orders from "./assets/data/orders.json";
+import { Entypo } from "@expo/vector-icons";
+const order = orders[0];
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<View>
+				<Image
+					source={{ uri: order.Restaurant.image }}
+					style={{ width: 50, height: 50 }}
+				/>
+				<View>
+					<Text>{order.Restaurant.name}</Text>
+					<Text>{order.Restaurant.address}</Text>
+					<Text>Delivery Details:</Text>
+					<Text>{order.User.name}</Text>
+					<Text>{order.User.address}</Text>
+				</View>
+				<Entypo name="check" size={30} color="black" />
+			</View>
+			<StatusBar style="auto" />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "#fff",
+		alignItems: "center",
+		justifyContent: "center",
+	},
 });
