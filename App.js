cd58428b5b604fc4039bdf609ec3113165aa0147
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-11-23 23:33:15 +0530

    cleaned card

--- App.js
+++ App.js
@@ -18,15 +18,7 @@ export default function App() {
 					source={{ uri: order.Restaurant.image }}
 					style={{ width: 50, height: 50 }}
 				/>
-				<View style={{ marginLeft: 10 }}>
-					<Text style={{ fontSize: 18, fontWeight: "500" }}>
-						{order.Restaurant.name}
-					</Text>
-					<Text style={{ color: "grey" }}>{order.Restaurant.address}</Text>
-					<Text style={{ marginTop: 10 }}>Delivery Details:</Text>
-					<Text style={{ color: "grey" }}>{order.User.name}</Text>
-					<Text style={{ color: "grey" }}>{order.User.address}</Text>
-				</View>
+				<View style={{ marginLeft: 10 }}></View>
 
 				<Entypo name="check" size={30} color="black" />
 			</View>
