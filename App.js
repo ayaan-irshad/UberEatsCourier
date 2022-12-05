ffb3e621486c141640be9afcbb2a5fc6e163a347
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-12-05 23:53:27 +0530

    refactor

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image } from "react-native";
 import orders from "./assets/data/orders.json";
-import { Entypo } from "@expo/vector-icons";
 const order = orders[0];
 export default function App() {
 	return <View style={styles.container}></View>;
