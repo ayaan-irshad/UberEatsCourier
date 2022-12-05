3c5543f3166b8b26e8c50b9a61325509adbad91d
Ayaan Irshad <54441228+ayaan-irshad@users.noreply.github.com>
2022-12-05 23:53:55 +0530

    cleaned app.jss

--- App.js
+++ App.js
@@ -1,15 +1,6 @@
 import { StyleSheet, Text, View, Image } from "react-native";
-import orders from "./assets/data/orders.json";
-const order = orders[0];
 export default function App() {
 	return <View style={styles.container}></View>;
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
+const styles = StyleSheet.create({});
